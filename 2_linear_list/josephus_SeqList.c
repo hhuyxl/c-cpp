0abe17a778931ce88c88f86c75272dfbdf74e255
hhuyxl <35727755+hhuyxl@users.noreply.github.com>
2020-09-11 10:22:59 +0800

    update

--- 2_linear_list/josephus_SeqList.c
+++ 2_linear_list/josephus_SeqList.c
@@ -49,5 +49,19 @@ int locate_seq(PSeqList palist, int x){
 //顺序表的插入
 /*在palist所指的顺序表中，下标为p的元素之前，插入一个值为x的元素，返回插入成功与否的标志*/
 int insertPre_seq(PSeqList palist, int p, int x){
-    
+    int q;
+    if(palist->n  >= palist->MAXNUM){
+        printf("Overflow！\n");
+        return 0;
+    }
+    if(p<0 || p>palist->n){
+        printf("Not exist!\n");
+        return 0;
+    }
+    for(q = palist->n - 1; q>=p; q--){
+        palist->element[q+1] = palist->element[q];
+    }
+    palist->element[p] = x;
+    palist->n = palist->n + 1;
+    return 1;
 }
