907ca6d1570d0143ee7820c05be96a7a2706a271
hhuyxl <35727755+hhuyxl@users.noreply.github.com>
2020-09-11 10:34:21 +0800

    idea 整合github项目测试

--- 2_linear_list/josephus_SeqList.c
+++ 2_linear_list/josephus_SeqList.c
@@ -65,3 +65,9 @@ int insertPre_seq(PSeqList palist, int p, int x){
     palist->n = palist->n + 1;
     return 1;
 }
+
+//顺序表的插入
+/*在palist所指的顺序表下，下标为p元素之后插入一个值为x的元素，返回插入成功与否的标志*/
+int insertPost_seq(PSeqList palist, int p, int x){
+
+}
